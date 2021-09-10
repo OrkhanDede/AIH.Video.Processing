70cd116a1a5783a1076d3f73a17b1a73c1d77604
orkhandede <vasoft01@gmail.com>
2021-09-10 17:13:41 +0400

    first commit

---
+++ AIH.DEMO.VideoStreaming/Controllers/FileUploadController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using AIH.DEMO.VideoStreaming.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
+
+namespace AIH.DEMO.VideoStreaming.Controllers
+{
+    public static class Constants
+    {
+        public const string ApiTemplate = "api/[controller]";
+    }
+
+    [ApiController]
+    [Route(Constants.ApiTemplate)]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly FileService _fileService;
+        private readonly string _targetFilePath;
+        private readonly string _targetFfmegeExePath;
+
+        public FileUploadController(FileService fileService, IConfiguration config)
+        {
+            _fileService = fileService;
+            _targetFilePath = config.GetValue<string>("StoredFilesPath");
+            _targetFfmegeExePath = config.GetValue<string>("FFMPEGExePath");
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("okay man");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadVideo()
+        {
+            //var request = HttpContext.Request;
+            //// validation of Content-Type
+            //// 1. first, it must be a form-data request
+            //// 2. a boundary should be found in the Content-Type
+            //if (!request.HasFormContentType ||
+            //    !MediaTypeHeaderValue.TryParse(request.ContentType, out var mediaTypeHeader) ||
+            //    string.IsNullOrEmpty(mediaTypeHeader.Boundary.Value))
+            //{
+            //    return new UnsupportedMediaTypeResult();
+            //}
+
+            //var reader = new MultipartReader(mediaTypeHeader.Boundary.Value, request.Body);
+            //var section = await reader.ReadNextSectionAsync();
+
+
+            //// This sample try to get the first file from request and save it
+            //// Make changes according to your needs in actual use
+            //while (section != null)
+            //{
+            //    var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+            //        out var contentDisposition);
+
+            //    if (hasContentDispositionHeader && contentDisposition.DispositionType.Equals("form-data") &&
+            //        !string.IsNullOrEmpty(contentDisposition.FileName.Value))
+            //    {
+            //        // Don't trust any file name, file extension, and file data from the request unless you trust them completely
+            //        // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
+            //        // In short, it is necessary to restrict and verify the upload
+            //        // Here, we just use the temporary folder and a random file name
+
+            //        // Get the temporary folder, and combine a random file name with it
+            //        var fileName = Path.GetRandomFileName();
+            //        var saveToPath = Path.Combine(_targetFilePath, $"{fileName}.mp4");
+
+            //        using (var targetStream = System.IO.File.Create(saveToPath))
+            //        {
+            //            await section.Body.CopyToAsync(targetStream);
+            //        }
+
+
+            //        return Ok();
+            //    }
+            //    section = await reader.ReadNextSectionAsync();
+            //}
+
+            //// If the code runs to this location, it means that no files have been saved
+            //return BadRequest("No files data in the request.");
+
+            await _fileService.ProcessVideo();
+            return Ok();
+        }
+
+        public void ProcessVideo()
+        {
+
+        }
+    }
+}
---
+++ AIH.DEMO.VideoStreaming/Services/FileService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Events;
+
+namespace AIH.DEMO.VideoStreaming.Services
+{
+    public class FileService
+    {
+        private readonly string _targetFFpegExePath;
+        private readonly string _targetFilePath;
+
+        public FileService(IConfiguration config)
+        {
+            _targetFilePath = config.GetValue<string>("StoredFilesPath");
+            _targetFFpegExePath = config.GetValue<string>("FFMPEGExePath");
+        }
+        public void UploadVideo(IFormFile video)
+        {
+
+        }
+
+        public async Task ProcessVideo()
+        {
+            var randomFileName = Path.GetRandomFileName();
+
+            FFmpeg.SetExecutablesPath(_targetFFpegExePath);
+            var inputFile = $"{_targetFilePath}\\2_3mg.mp4";
+            var outputFile = $"{_targetFilePath}\\processed\\{randomFileName}_output.mp4";
+
+
+            IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(inputFile);
+
+            IStream videoStream = mediaInfo.VideoStreams.FirstOrDefault()?.SetCodec(VideoCodec.h264);
+
+
+
+            await FFmpeg.Conversions.New().
+                AddStream(videoStream).
+                AddParameter($"-map 0").
+                AddParameter("-c copy").
+                AddParameter($"-hls_time 10").
+                AddParameter($"-hls_playlist_type vod").
+                AddParameter($"-hls_segment_filename {_targetFilePath}\\processed\\32s_new\\30s_new_%03d.ts {_targetFilePath}\\processed\\32s_new\\30s_new.m3u8").
+                //SetOutput(outputFile).
+                //SetOutputFormat(Format.mp4).
+
+                Start();
+
+        }
+
+
+        public void OnPregress(ConversionProgressEventHandler e)
+        {
+
+        }
+    }
+}
